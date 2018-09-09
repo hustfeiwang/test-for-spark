f62deba2162b182c13da6039ee631c459da9fdd0
feiwang <275254920@qq.com>
2018-09-09 14:28:58 +0800

    add parameters and mkdir logs

--- upload-data.sh
+++ upload-data.sh
@@ -2,7 +2,7 @@
 bin=`dirname $0`
 bin=`cd $bin;pwd`
 source $bin/conf.sh
-
+mkdir $bin/logs
 #放置在SPARK_HOME下面用于测试examples的小数据，是用于mllib
 $HADOOP_HOME/bin/hadoop fs -mkdir /user/$USER_NAME
 $HADOOP_HOME/bin/hadoop fs -put $SPARK_HOME/data /user/$USER_NAME
